b31d0b568684be6d7f406cb5c07e290e67a5e5ed
Dave Kapell <david@rigitech.com>
2023-12-21 22:02:43 -0500

    add httpAlways

--- doc/campaigns.sql
+++ doc/campaigns.sql
@@ -1,1 +1,1 @@
-insert into campaigns (name, default_site, created_by) values ('default', true, 1);
+insert into campaigns (name, default_site, created_by, theme, description) values ('default', true, 1, 'Flatly', 'Default Site');

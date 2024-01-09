0de9806b2ffa1a61748d92629f497468efc4e4e5
Dave Kapell <david@rigitech.com>
2024-01-09 11:49:46 -0500

    sort tags consistently

--- models/skill_tag.js
+++ models/skill_tag.js
@@ -5,7 +5,21 @@ const Model = require('../lib/Model');
 const tableFields = ['id', 'campaign_id', 'name', 'description', 'display_to_pc', 'on_sheet', 'color', 'type'];
 
 const SkillTag = new Model('skill_tags', tableFields, {
-    validator: validate
+    validator: validate,
+    sorter: function(a,b){
+        if (a.type === 'category'){
+            if (b.type === 'category'){
+                return a.type.localeCompare(b.type);
+            }
+            return -1;
+        } else if (b.type === 'category'){
+            return 1;
+        } else if (a.type !== b.type){
+            return a.type.localeCompare(b.type);
+        } else {
+            return a.name.localeCompare(b.name);
+        }
+    }
 });
 
 module.exports = SkillTag;

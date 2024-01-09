2d892c4455afff3ed802bb0f3e6578acb71fa818
Dave Kapell <dave.kapell@delphix.com>
2024-01-08 20:10:12 -0500

    skills validator

---
+++ public/javascripts/skill-validate.js
@@ -0,0 +1,34 @@
+/* globals _ marked */
+'use strict';
+
+$(function(){
+    $('.validator-select').on('change', updateValidationDisplay);
+    updateValidationDisplay();
+});
+
+function updateValidationDisplay(){
+    const options = {};
+    $('.validator-select').each( function (e){
+        const $this = $(this);
+        const checked = !!$this.prop('checked');
+        const type = $this.data('type');
+        options[type] = checked;
+    });
+
+    $('#skill-validations').find('.list-group-item').each( function(e){
+        const $this = $(this);
+        let show = false;
+        for (const type of $this.data('issues').split(',')){
+            if (options[type]){
+                console.log('showing because ' + type)
+                show = true;
+            }
+        }
+        if (show){
+            $this.show();
+        } else {
+            $this.hide();
+        }
+
+    });
+}

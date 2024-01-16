1f5efc1305d780dbc86e22c48a6877b650f06d5b
Dave Kapell <david@rigitech.com>
2024-01-15 23:50:33 -0500

    re-enable templates

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,16 +2,16 @@
 
 # build assorted templates for client side
 # Skills
-#./node_modules/pug-cli/index.js -c --name newFormTemplate -o public/javascripts/templates/skill views/skill/modalNewForm.pug
-#./node_modules/pug-cli/index.js -c --name editFormTemplate -o public/javascripts/templates/skill views/skill/modalEditForm.pug
+./node_modules/pug-cli/index.js -c --name newFormTemplate -o public/javascripts/templates/skill views/skill/modalNewForm.pug
+./node_modules/pug-cli/index.js -c --name editFormTemplate -o public/javascripts/templates/skill views/skill/modalEditForm.pug
 
 # Characters
-#./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/character views/character/templates/*.pug
-#./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates views/clientPartials/*.pug
+./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/character views/character/templates/*.pug
+./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates views/clientPartials/*.pug
 
 # Reports
-#./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/report/group views/report/templates/group/*.pug
-#./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/report/skill views/report/templates/skill/*.pug
+./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/report/group views/report/templates/group/*.pug
+./node_modules/pug-cli/index.js -c --name-after-file -o public/javascripts/templates/report/skill views/report/templates/skill/*.pug
 
 # Custom Forms
 ./node_modules/pug-cli/index.js -c --name optionDescriptionTemplate -o public/javascripts/templates/custom_form views/character_field/optionDescriptionForm.pug

1c258d0bebec0ef9171ea69e9ba5ab33e2a6b66c
Dave Kapell <david@rigitech.com>
2024-11-07 10:38:38 -0500

    fix front page when not in admin mode

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,7 +16,7 @@ async function showIndex(req, res){
         if (req.session.player_mode){
             user.type = 'player';
         }
-        if (user && user.type === 'player' && !user.site_admin){
+        if (user && user.type === 'player' && !req.session.admin_mode){
             const characterData = await req.models.character.findOne({user_id: user.id, active: true, campaign_id:req.campaign.id});
             if (characterData){
                 const character = new Character({id:characterData.id});

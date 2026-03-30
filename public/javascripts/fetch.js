6e5d6fe0dddff9575e038a5145591b6e4902c2f5
Dave Kapell <david@rigitech.com>
2026-03-30 16:43:43 -0400

    improve csrf handling

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -1,15 +1,14 @@
 async function fetchWithCsrfRetry(url, options = {}, retries = 1){
-    options.credentials = 'same-origin';
     let response = await fetch(url,options);
     if (response.status === 403 && retries > 0) {
         console.warn('CSRF token mismatch, retrying...');
 
         const csrfResponse = await fetch('/api/csrf');
         const csrfTokenData =  await csrfResponse.json();
-        if (options.header){
-            options.header['x-csrf-token'] = csrfTokenData.csrfToken;
+        if (options.headers){
+            options.headers['x-csrf-token'] = csrfTokenData.csrfToken;
         } else {
-            options.header = {
+            options.headers = {
                 'x-csrf-token':  csrfTokenData.csrfToken
             };
         }

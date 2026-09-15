55f67d9fe9fa1723fee896375cfa0d906a564dbd
Dave Kapell <david@rigitech.com>
2026-09-14 23:09:56 -0400

    add debug

--- src/lib/scheduler/Schedule.ts
+++ src/lib/scheduler/Schedule.ts
@@ -482,6 +482,9 @@ class Schedule extends EventEmitter {
                                     if (_.indexOf(conflicts, prereqScene.id) === -1){
                                         conflicts.push(prereqScene.id);
                                     }
+                                    if (scene.name === 'A14 S7 - of substance, not ornament' ){
+                                        console.log(`continue - timeslot before prereq ${prereqScene.name}`)
+                                    }
                                     continue timeslotLoop;
                                 }
                             }
@@ -490,6 +493,9 @@ class Schedule extends EventEmitter {
 
                     // check if Required Staff and Players are available starting at this timeslot
                     if (! await this.checkTimeslotUsers(scene, timeslotId, _.indexOf(sceneTimeslots.during, checkTimeslotId) === -1)){
+                        if (scene.name === 'A14 S7 - of substance, not ornament' ){
+                            console.log(`continue - not everyone available`)
+                        }
                         continue timeslotLoop;
                     }
 
@@ -500,9 +506,15 @@ class Schedule extends EventEmitter {
                                 foundTimeslots.push(timeslots[timeslotIdx+idx].id);
                             }
                             scene.currentLocations = suggestedLocations;
+                            if (scene.name === 'A14 S7 - of substance, not ornament' ){
+                                console.log(`done, found location}`)
+                            }
                             break timeslotLoop;
                         }
                     }
+                }
+                if (scene.name === 'A14 S7 - of substance, not ornament' ){
+                    console.log(`reloop`)
                 }
             }
         }

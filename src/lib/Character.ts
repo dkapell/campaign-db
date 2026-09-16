816167b1913c5177aa80d002a088ea13baec66a6
Dave Kapell <david@rigitech.com>
2026-09-16 10:49:45 -0400

    more debug for character

--- src/lib/Character.ts
+++ src/lib/Character.ts
@@ -487,6 +487,10 @@ class Character{
 
         doc.provides = await gatherProvides(doc.sources, false, null);
         doc.provides = await gatherProvides(doc.skills, true, doc.provides);
+
+        for (const skill of doc.provides.skills){
+            console.log(`${skill.name}: ${_.pluck(skill.tags, 'name').join(', ')}`)
+        }
         doc.provides.attributes = await this.prepAttributes(doc.provides.attributes);
         doc.provides.internalAttributes = [];
         for (const attribute of doc.provides.attributes){
